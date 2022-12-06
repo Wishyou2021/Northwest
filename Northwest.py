c2181110854eccd2dc0dcb115b5d47e89f036780
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-06 11:42:02 +0800

    Update Northwest.py

--- Northwest.py
+++ Northwest.py
@@ -4,14 +4,14 @@ st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
 option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', 'SOUTHEAST', 'NORTHWEST','太平洋組','西南組'])
 if option=='NORTHWEST':
-  teams=st.sidebar.selectbox( '選擇球隊？', ['Atlanta Hawks', 'Charlotte Hornets', 'Miami Heat', 'Orlando Magic','Washington Wizards'])
-  if teams=='Atlanta Hawks':
-    NORTHWEST.AtlantaHawks()
-  if teams=='Charlotte Hornets':
-    NORTHWEST.CharlotteHornets()
-  if teams=='Miami Heat':
-    NORTHWEST.MiamiHeat()
-  if teams=='Orlando Magic':
-    NORTHWEST.OrlandoMagic()
-  if teams=='Washington Wizards':
-    NORTHWEST.WashingtonWizards()
+  teams=st.sidebar.selectbox( '選擇球隊？', ['Denver Nuggets', 'Minnesota Timberwolves', 'Oklahoma City Thunder', 'Portland Trail Blazers','Utah Jazz'])
+  if teams=='Denver Nuggets':
+    NORTHWEST.DenverNuggets()
+  if teams=='Minnesota Timberwolves':
+    NORTHWEST.MinnesotaTimberwolves()
+  if teams=='Oklahoma City Thunder':
+    NORTHWEST.OklahomCityThunder()
+  if teams=='Portland Trail Blazers':
+    NORTHWEST.PortlandTrailBlazers()
+  if teams=='Utah Jazz':
+    NORTHWEST.UtahJazz()
