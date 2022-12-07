f6963285ec4058eea3041782d91c29084d6a061e
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-07 11:25:38 +0800

    Update Northwest.py

--- Northwest.py
+++ Northwest.py
@@ -8,16 +8,16 @@ if option=='NORTHWEST':
   teams=st.sidebar.selectbox( '選擇球隊？', ['Denver Nuggets', 'Minnesota Timberwolves', 'Oklahoma City Thunder', 'Portland Trail Blazers','Utah Jazz'])
   if teams=='Denver Nuggets':
     NORTHWEST.DenverNuggets()
-    NORTHWEST.DenverNuggets_map()
+    NORTHWEST_map.DenverNuggets_map()
   if teams=='Minnesota Timberwolves':
     NORTHWEST.MinnesotaTimberwolves()
-    NORTHWEST.MinnesotaTimberwolves_map()
+    NORTHWEST_map.MinnesotaTimberwolves_map()
   if teams=='Oklahoma City Thunder':
     NORTHWEST.OklahomCityThunder()
-    NORTHWEST.OklahomCityThunder_map()
+    NORTHWEST_map.OklahomCityThunder_map()
   if teams=='Portland Trail Blazers':
     NORTHWEST.PortlandTrailBlazers()
-    NORTHWEST.PortlandTrailBlazers_map()
+    NORTHWEST_map.PortlandTrailBlazers_map()
   if teams=='Utah Jazz':
     NORTHWEST.UtahJazz()
-    NORTHWEST.UtahJazz_map()
+    NORTHWEST_map.UtahJazz_map()
