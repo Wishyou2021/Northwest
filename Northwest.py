559456d60a96726080b54be149c7262c45d90938
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-12-13 19:32:06 +0800

    Update Northwest.py

--- Northwest.py
+++ Northwest.py
@@ -1,6 +1,7 @@
 import streamlit as st  
 import NORTHWEST 
 import NORTHWEST_map
+import Northwest_Star
 st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
 option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', 'SOUTHEAST', 'NORTHWEST','太平洋組','西南組'])
@@ -9,15 +10,20 @@ if option=='NORTHWEST':
   if teams=='Denver Nuggets':
     NORTHWEST.DenverNuggets()
     NORTHWEST_map.DenverNuggets_map()
+    Northwest_Star.DenverNuggets_Star()
   if teams=='Minnesota Timberwolves':
     NORTHWEST.MinnesotaTimberwolves()
     NORTHWEST_map.MinnesotaTimberwolves_map()
+    Northwest_Star.MinnesotaTimberwolves_Star()
   if teams=='Oklahoma City Thunder':
     NORTHWEST.OklahomCityThunder()
     NORTHWEST_map.OklahomCityThunder_map()
+    Northwest_Star.OklahomCityThunder_Star()
   if teams=='Portland Trail Blazers':
     NORTHWEST.PortlandTrailBlazers()
     NORTHWEST_map.PortlandTrailBlazers_map()
+    Northwest_Star.PortlandTrailBlazers_Star()
   if teams=='Utah Jazz':
     NORTHWEST.UtahJazz()
     NORTHWEST_map.UtahJazz_map()
+    Northwest_Star.UtahJazz_Star()
